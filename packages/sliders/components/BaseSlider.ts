b51253376ceea460268e510e3fb81a664021e9bd
Paul Mason <paul.mason@outlook.com>
2022-06-26 15:58:17 +0200

    Added base properties.

--- packages/sliders/components/BaseSlider.ts
+++ packages/sliders/components/BaseSlider.ts
@@ -1,6 +1,19 @@
 import { LitElement } from "lit";
-//import { property } from 'lit/decorators.js';
+import { property } from 'lit/decorators.js';
 
 export abstract class BaseSlider extends LitElement {
+    @property({ type: Object })
+    public value: null | number | object;
 
+    @property({ type: Object })
+    public min: null | number | { x: number, y: number } = null;
+
+    @property({ type: Object })
+    public max: null | number | { x: number, y: number } = null;
+
+    @property({ type: Object })
+    public step: null | number | 'any' | { x: number | 'any', y: number | 'any' } = null;
+
+    @property({ type: Object })
+    public valueMap: null | string | { x: string, y: string }
 }
